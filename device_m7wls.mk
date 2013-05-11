cdbe68d2a920268c627fa80f5a15548467148287
Ethan Chen <intervigil@gmail.com>
2013-05-10 19:39:10 -0700

    m7wls: move ueventd to m7-common, consolidate init scripts
    Change-Id: I852d48571362a6fd8b4c8b90b81dd6340e4f879f

--- device_m7wls.mk
+++ device_m7wls.mk
@@ -19,13 +19,6 @@ $(call inherit-product, device/htc/m7-common/m7-common.mk)
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# Boot ramdisk setup
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/root/fstab.htc:root/fstab.htc \
-    $(LOCAL_PATH)/root/remount.htc:root/remount.htc \
-    $(LOCAL_PATH)/root/init.m7wls.rc:root/init.m7wls.rc \
-    $(LOCAL_PATH)/root/ueventd.m7wls.rc:root/ueventd.m7wls.rc \
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
