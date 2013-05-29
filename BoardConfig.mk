72cf4c9ef535d77e32579d14898e498b4d576b81
Ethan Chen <intervigil@gmail.com>
2013-05-29 14:26:53 -0700

    m7spr: allow m7wls devices
    Change-Id: I01bdd1b69863c2210cc93e82b7057d45c9fad635

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@
 -include device/htc/m7-common/BoardConfigCommon.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := m7spr
+TARGET_OTA_ASSERT_DEVICE := m7wls,m7spr
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := m7wls
