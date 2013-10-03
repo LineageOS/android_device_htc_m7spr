3b780923561d42c8f6cc2b2f3949c682cd248796
Kyle Laker <10kylelaker@gmail.com>
2013-10-02 21:20:00 -0400

    m7spr: Correct manufacturer, htc to HTC
    Change htc for m7spr manufacter to HTC to make the string look cleaner
    in Android Device Manager and Google Play Store (commonly used by users),
    as well as any app that reads ro.product.manufacturer from build.prop.
    Also, HTC styles their name in print as being uppercase and generally have
    it uppercase in their stock ROMs.
    
    Change-Id: I7a7ea1d0b9be2e8c0054446e6d36dad8f6298e76

--- full_m7spr.mk
+++ full_m7spr.mk
@@ -32,5 +32,5 @@ $(call inherit-product, device/htc/m7spr/device.mk)
 PRODUCT_NAME := full_m7spr
 PRODUCT_DEVICE := m7spr
 PRODUCT_BRAND := htc
-PRODUCT_MANUFACTURER := htc
+PRODUCT_MANUFACTURER := HTC
 PRODUCT_MODEL := One
