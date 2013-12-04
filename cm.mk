2cb6288903ea752df7b3e532d6960fbb34db835a
Dorian Snyder <dastin1015@gmail.com>
2013-12-04 00:41:25 -0800

    m7spr: update fingerprint to latest OTA
    Change-Id: I9c9183e7623c6367d405cc943a1be0524fb36f37

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wls BUILD_ID=JZO54K BUILD_FINGERPRINT="htc/m7wls/m7wls:4.1.2/JZO54K/166937.7:user/release-keys" PRIVATE_BUILD_DESC="1.29.651.7 CL166937 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wls BUILD_ID=JSS15J BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:4.3/JSS15J/251863.2:user/release-keys" PRIVATE_BUILD_DESC="3.04.651.2 CL251863 release-keys"
 
 PRODUCT_NAME := cm_m7spr
 PRODUCT_DEVICE := m7spr
