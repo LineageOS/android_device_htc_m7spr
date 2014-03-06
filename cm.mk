00979b4d1c47be0f9f53dbea9d08847c4a2c6150
Markus Guidry <markus@gu1dry.com>
2014-03-06 16:42:17 -0500

    minor: Align ro.product with stock
      PS2: Add in missing trailing slashes.
    
    Change-Id: I81deb4bfaa7cde4ac9a0808cce49b9f753f3cf5b

--- cm.mk
+++ cm.mk
@@ -4,11 +4,13 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/m7spr/full_m7spr.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=sprint_wwe \
-    BUILD_ID=KOT49H \
-    BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:4.4.2/KOT49H/303687.4:user/release-keys" \
-    PRIVATE_BUILD_DESC="4.06.651.4 CL303687 release-keys"
-
 PRODUCT_NAME := cm_m7spr
 PRODUCT_DEVICE := m7spr
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL=HTCONE \
+    PRODUCT_NAME=sprint_wwe \
+    PRODUCT_DEVICE=m7wls \
+    BUILD_ID=KOT49H \
+    PRIVATE_BUILD_DESC="4.06.651.4 CL303687 release-keys" \
+    BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:4.4.2/KOT49H/303687.4:user/release-keys"
