7779c60169a56a51d44d3cc2b341e2fc9655f8a7
Kyle Laker <10kylelaker@gmail.com>
2014-09-23 20:10:02 -0400

    m7spr: Add FM radio support
    This change depends on:
    http://review.cyanogenmod.org/#/c/73723/
    http://review.cyanogenmod.org/#/c/73884/
    
    Change-Id: If68c16f9fca62b6436803dd2cda967b6304f63a9

--- device.mk
+++ device.mk
@@ -20,5 +20,13 @@ $(call inherit-product-if-exists, vendor/htc/m7spr/m7spr-vendor.mk)
 # local overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# FM radio
+PRODUCT_PACKAGES += \
+    qcom.fmradio \
+    libqcomfm_jni \
+    FM2 \
+    FMRecord
+
+
 # Inherit from m7-common
 $(call inherit-product, device/htc/m7-common/m7-common.mk)
