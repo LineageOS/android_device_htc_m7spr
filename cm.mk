541e9ca8656a300e25154a7c853d00c93a7e3e32
David Hays <dhays90@gmail.com>
2015-02-16 19:58:17 -0600

    m7spr: Update fingerprint
    Change-Id: Ie9ce33e929e2012c71eb77c04fdb9eb86b52d105

--- cm.mk
+++ cm.mk
@@ -6,9 +6,9 @@ $(call inherit-product, device/htc/m7spr/full_m7spr.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sprint_wwe \
-    BUILD_ID=KOT49H \
-    BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:4.4.2/KOT49H/337390.3:user/release-keys" \
-    PRIVATE_BUILD_DESC="5.03.651.3 CL337390 release-keys"
+    BUILD_ID=LRX22F \
+    BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:5.0.2/LRX22F/461956.2:user/release-keys" \
+    PRIVATE_BUILD_DESC="6.16.651.2 CL461956 release-keys"
 
 PRODUCT_NAME := cm_m7spr
 PRODUCT_DEVICE := m7spr
